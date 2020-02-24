171b87c506d6cab4507cd2d21fcdc7fc30d5b813
Younes <younesberrada1@gmail.com>
2020-02-24 15:04:55 +0100

    RED

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.use('/user', user);
 app.use('/location', loc);
 app.use('/color',color_route);
 
+var color = 'rgb(197, 33, 33)';  //red
+
+app.use('/color',color);
+
+//  var color = 'rgb(59, 108, 170)' //blue
+
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
